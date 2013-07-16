7937348eecbfb6cae4ae3c64498aa3610a599bae
osvin admin <osvinadmin@osvin-admins-imac.local>
2013-07-16 17:49:29 +0530

    Initial Commit

---
+++ DinnerPlace/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  DinnerPlace
+//
+//  Created by osvin admin on 7/16/13.
+//  Copyright (c) 2013 osvin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
